d172c31521156a03fdb392b13fef7a330d7bc88d
htchoi1995 <htchoi1995@gmail.com>
2014-11-12 14:07:51 -0500

    Disable/Enable screenshot sound [1/2]
    Change-Id: I34df3a37ac7e71e2d89276295ebf4330190e75aa
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4713,6 +4713,12 @@ public final class Settings {
         private static final Validator GESTURE_PILL_TOGGLE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
