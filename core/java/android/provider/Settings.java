45264bb6e56bae586b6450382accc42c9c05f9b9
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    HeadsUp: add timeout option (1/2)
    Forward ported to marshmallow/nougat By: BeansTown106
    this was basically a rewrite as everything about it changed
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 15 Apr 2022 21:20:04 +0530
        Subject: HeadsUp: Change heads up timeout to seconds
    
    Change-Id: I9c88d67d25440dff8f545e1330da672be5d36913
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6837,6 +6837,12 @@ public final class Settings {
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
