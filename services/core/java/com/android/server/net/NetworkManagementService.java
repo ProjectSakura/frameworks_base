e0bb9f149d504569b88a72bf47eeed3bc548a1a6
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-19 20:04:04 +0100

    AppProfiler: dont be so hectic on cpu battery stats
    Also disable cpu monitor
    TheHardGamer: Port to A13
    
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- services/core/java/com/android/server/net/NetworkManagementService.java
+++ services/core/java/com/android/server/net/NetworkManagementService.java
@@ -120,7 +120,7 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
     }
 
     private static final String TAG = "NetworkManagement";
-    private static final boolean DBG = Log.isLoggable(TAG, Log.DEBUG);
+    private static final boolean DBG = false;
 
     /**
      * Binder context for this service
