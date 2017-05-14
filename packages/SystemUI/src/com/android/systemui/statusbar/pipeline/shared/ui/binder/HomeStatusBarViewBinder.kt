a2c9d4e60aef201e3b07ee60e657f072e59796ac
Pranav Vashi <neobuddy89@gmail.com>
2017-05-14 08:55:06 +0530

    Custom statusbar logo customizations [1/2]
    * Original placement logic and custom logo patches
      by LorDClockaN and varund7726 respectively.
    * Refresh and resize various icons.
    
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 7 Jul 2024 18:03:28 +0530
        Subject: SystemUI: Fix multiuser usage for custom logo
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
@@ -34,6 +34,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.android.app.animation.Interpolators
 import com.android.systemui.sakura.batterybar.BatteryBarController
+import com.android.systemui.sakura.logo.LogoImage
 import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent.PerDisplaySingleton
 import com.android.systemui.lifecycle.repeatWhenAttached
 import com.android.systemui.res.R
@@ -140,6 +141,7 @@ constructor(
         val rightPaddingInit = rightClock.capturePadding()
 
         val batteryBar: BatteryBarController = view.requireViewById(R.id.battery_bar)
+        val leftLogo: LogoImage = view.requireViewById(R.id.statusbar_logo)
 
         // CollapsedStatusBarFragment doesn't need this
         if (StatusBarRootModernization.isEnabled) {
@@ -150,6 +152,7 @@ constructor(
             centerClock.hideInitially(state = View.GONE)
             rightClock.hideInitially(state = View.GONE)
             batteryBar.hideInitially()
+            leftLogo.hideInitially()
             notificationIconsArea.hideInitially()
         }
 
@@ -524,6 +527,7 @@ constructor(
                         viewModel.isNotificationIconContainerVisible.collect {
                             notificationIconsArea.adjustVisibility(it)
                             batteryBar.adjustVisibility(it)
+                            leftLogo.adjustVisibility(it)
                         }
                     }
 
