d4ef9baf6c2399a3a8b0600d256e86c8b5a096bf
Rituj Beniwal <ritujbeniwal@gmail.com>
2017-11-09 17:31:07 +0530

    base: Screen Stabiization [1/2]
    * New Settings menu integrated into QS itself(long click)
    * Thanks to @SuperDroidBond for keeping this alive till Pie
    
    Change-Id: Ibccb8d78d79938f42241ea3690ea66c3aace89ea
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    
    ScreenStabilization Tile: Fix layout
    
    Signed-off-by: rituj <ritujbeniwal@gmail.com>
    Change-Id: I973fd8dbef971ed726ebcf323fbfb35aed4495bf
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -57,6 +57,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
+import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.VpnTile;
@@ -107,6 +108,8 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<VpnTile> mVpnTileProvider;
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
     private final Provider<FPSInfoTile> mFPSInfoTileProvider;
+    private final Provider<ScreenStabilizationTile> mScreenStabilizationTileProvider;
+
     private QSTileHost mHost;
 
     @Inject
@@ -144,6 +147,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<DataSwitchTile> dataSwitchTileProvider,
             Provider<AODTile> aodTileProvider,
             Provider<FPSInfoTile> fpsInfoTileProvider) {
+            Provider<ScreenStabilizationTile> screenStabilizationTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -178,6 +182,7 @@ public class QSFactoryImpl implements QSFactory {
         mVpnTileProvider = vpnTileProvider;
         mDataSwitchTileProvider = dataSwitchTileProvider;
         mFPSInfoTileProvider = fpsInfoTileProvider;
+        mScreenStabilizationTileProvider = screenStabilizationTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -264,6 +269,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mDataSwitchTileProvider.get();
             case "fpsinfo":
                 return mFPSInfoTileProvider.get();
+            case "screenstabilization":
+                return mScreenStabilizationTileProvider.get();
         }
 
         // Intent tiles.
