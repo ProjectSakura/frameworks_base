418a778ae8c14ea557ac7809bfab474d690fab74
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2017-12-28 21:55:45 +0100

    Allow screen unpinning on devices without navbar
    Change-Id: Iedfc08f4d95bbee3c8578c0d2450b90739e63603
    
    Screen Pinning: Show correct text for on screen nav.
    
      Similar to I09c2ef661bff272cb4f7ca43bac0e45f4b20a4d4,
      we're not getting an instance of PhoneWindowManager which
      we can rely on to update dynamically.
    
      TICKET: OPO-393
    
    Change-Id: Iacf8221066461fb6940dd88432e665812545c3ff
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1476,7 +1476,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
                 "Back - Long Press");
-        performKeyAction(mBackLongPressAction, event);
+        if (!unpinActivity(false)) {
+            performKeyAction(mBackLongPressAction, event);
+        }
     }
 
     private void accessibilityShortcutActivated() {
@@ -1532,7 +1534,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private boolean hasLongPressOnBackBehavior() {
-        return mBackLongPressAction != Action.NOTHING;
+        return mBackLongPressAction != Action.NOTHING || unpinActivity(true);
     }
 
     private void interceptScreenshotChord() {
@@ -3611,6 +3613,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
             } catch (Exception e) {
                 Slog.w(TAG, "Could not dispatch event to device key handler", e);
+            }
+        }
+        return false;
+    }
+
+    private boolean unpinActivity(boolean checkOnly) {
+        if (!hasNavigationBar()) {
+            try {
+                if (ActivityTaskManager.getService().isInLockTaskMode()) {
+                    if (!checkOnly) {
+                        ActivityTaskManager.getService().stopSystemLockTaskMode();
+                    }
+                    return true;
+                }
+            } catch (RemoteException e) {
+                // ignore
             }
         }
         return false;
