598da412c54912a4e92a74377d802d6b9e251657
varund7726 <date.varun123@gmail.com>
2019-07-11 11:04:15 +0300

    SystemUI: Option to disable Data Disabled Indicator icon [1/2]
    [someone5678] Adapt to SystemUI mobile connections pipeline
    
    Change-Id: I4e5b058d9a19070494659f52be8d95892b0eaf5d
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: anoosragh69 <ok.ano0s6090@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6250,6 +6250,12 @@ public final class Settings {
         public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
 
         /**
+         * Statusbar logo
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
