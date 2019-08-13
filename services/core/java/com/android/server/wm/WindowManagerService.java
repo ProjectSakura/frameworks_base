f3465dad222464aa315511750abb0f9b5be9a3c1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-13 09:08:00 -0300

    fwb: Port long screenshot implementation from Asus
    From asus/WW_I001D/ASUS_I001_1:10/QKQ1.190825.002/17.0210.2001.60-0:user/release-keys
    
    Change-Id: Idd3690832210b167d9d2a33069b19ffb43653887
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7827,4 +7827,9 @@ public class WindowManagerService extends IWindowManager.Stub
                     0 /* configChanges */, !PRESERVE_WINDOWS, true /* notifyClients */);
         }
     }
+
+    @Override
+    public void takeAlternativeScreenshot() {
+        mPolicy.takeAlternativeScreenshot();
+    }
 }
