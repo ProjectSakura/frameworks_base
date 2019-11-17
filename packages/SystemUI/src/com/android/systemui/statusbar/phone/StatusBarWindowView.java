006c2f5f7c68a10ffa5f04adda0f6f8f3f7069e4
maxwen <max.weninger@gmail.com>
2019-11-18 00:29:15 +0100

    SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    Change-Id: I8ca091471e78bf1624935c7ea7444e93fc89a8ce
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowView.java
@@ -131,6 +131,7 @@ public class StatusBarWindowView extends FrameLayout {
     private static ImageButton mDismissAllButton;
     private static boolean wasHideAnimationAlreadyCalled = false;
     private static boolean wasShowAnimationAlreadyCalled = false;
+    private boolean mDoubleTapEnabledNative;
 
     private final GestureDetector.SimpleOnGestureListener mGestureListener =
             new GestureDetector.SimpleOnGestureListener() {
@@ -154,7 +155,7 @@ public class StatusBarWindowView extends FrameLayout {
                 }
                 return true;
             }
-            if (mDoubleTapEnabled || mSingleTapEnabled) {
+            if (mDoubleTapEnabled || mSingleTapEnabled || mDoubleTapEnabledNative) {
                 mService.wakeUpIfDozing(SystemClock.uptimeMillis(), StatusBarWindowView.this,
                         "DOUBLE_TAP");
                 return true;
@@ -165,6 +166,9 @@ public class StatusBarWindowView extends FrameLayout {
     private final TunerService.Tunable mTunable = (key, newValue) -> {
         AmbientDisplayConfiguration configuration = new AmbientDisplayConfiguration(mContext);
         switch (key) {
+            case Settings.Secure.DOUBLE_TAP_TO_WAKE:
+                mDoubleTapEnabledNative = TunerService.parseIntegerSwitch(newValue, false);
+                break;
             case Settings.Secure.DOZE_DOUBLE_TAP_GESTURE:
                 mDoubleTapEnabled = configuration.doubleTapGestureEnabled(UserHandle.USER_CURRENT);
                 break;
@@ -193,6 +197,7 @@ public class StatusBarWindowView extends FrameLayout {
         mGestureDetector = new GestureDetector(context, mGestureListener);
         mStatusBarStateController = Dependency.get(StatusBarStateController.class);
         Dependency.get(TunerService.class).addTunable(mTunable,
+                Settings.Secure.DOUBLE_TAP_TO_WAKE,
                 Settings.Secure.DOZE_DOUBLE_TAP_GESTURE,
                 Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
                 DOUBLE_TAP_SLEEP_GESTURE);
