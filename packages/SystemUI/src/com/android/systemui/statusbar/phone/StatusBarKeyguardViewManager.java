51bfb680172035936e47abe3f031a16d4b637bcd
Ali B <abittin@gmail.com>
2019-11-20 00:59:12 +0300

    SystemUI: hide lock icon while dozing
    As we cannot unlock while in doze state, makes no
    sense to have the lockicon shown in AD.
    
    This will also prevent the undesirable effect of
    burn-in-protection making small clock overlap
    with lockicon when pixels are shifted upwards.
    
    https://imgur.com/WfusHKS
    
    Change-Id: I9647f2f04b70b00a148cd8a645af0121810de425
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -874,6 +874,9 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
             updateNavigationBarVisibility(navBarVisible);
         }
 
+        mLockIconContainer.setVisibility((mLastLockVisible && mDozing)
+                 ? View.GONE : View.VISIBLE);
+
         if (bouncerShowing != mLastBouncerShowing || mFirstUpdate) {
             mNotificationShadeWindowController.setBouncerShowing(bouncerShowing);
             mStatusBar.setBouncerShowing(bouncerShowing);
