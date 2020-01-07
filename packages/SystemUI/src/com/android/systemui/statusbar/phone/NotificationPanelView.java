5c080926f069ec5317db7c517bad0435d46f87f0
00day0 <therandomuser11@gmail.com>
2020-01-07 20:15:27 +0100

    Blur behind quick settings thanks to @Dil3mm4 utility which allows to take screenshots and blur them
    todo:
    - fix blur in landscape
    - if you pull down quicksettings on lockscreen StatusBarState changed from KEYGUARD even tho technically we are still on lockscreen, change that check to something else
    
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2266,6 +2266,7 @@ public class NotificationPanelView extends PanelView implements
             alpha *= mClockPositionResult.clockAlpha;
         }
         mNotificationStackScroller.setAlpha(alpha);
+        mStatusBar.updateBlurVisibility();
     }
 
     private float getFadeoutAlpha() {
