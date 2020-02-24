92ce1d9a9e29ba7d5f5d04a72b03d3c3db0dcd54
AnkitGourav <Ankitgourav035@gmail.com>
2020-02-25 00:33:21 +0530

    SystemUI: Ambient Edge Pulse layout [1/2]
    - option to choose between faded & solid view
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4850,6 +4850,12 @@ public final class Settings {
 
 
         /**
+         * Ambient Edge Light Layout
+         * @hide
+         */
+        public static final String PULSE_AMBIENT_LIGHT_LAYOUT = "pulse_ambient_light_layout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
