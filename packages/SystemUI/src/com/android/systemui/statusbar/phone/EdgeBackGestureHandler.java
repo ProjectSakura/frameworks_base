75c047979c2bf80c662b35aae145dafc04def823
LordShenron <shen.priyanshu@gmail.com>
2020-03-27 10:28:42 +0000

    Remove duplicate definitions of functions
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -268,9 +268,6 @@ public class EdgeBackGestureHandler implements DisplayListener, TunerService.Tun
         mIsInTransientImmersiveStickyState =
                 (systemUiVisibility & SYSTEM_UI_FLAG_IMMERSIVE_STICKY) != 0
                 && (systemUiVisibility & NAVIGATION_BAR_TRANSIENT) != 0;
-
-    public void onSettingsChanged() {
-        updateEdgeHeightValue();
     }
 
     private void disposeInputChannel() {
