422aeb0a15342502c0efa96958d1d5c657760c7d
LordShenron <shen.priyanshu@gmail.com>
2020-04-15 13:14:29 +0000

    Revert "base: Port extended screenshot function from OOS"
    This reverts commit f17f243d290e9858f99224ce17f438e5757d5dee.
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- services/core/java/com/android/server/policy/WindowManagerPolicy.java
+++ services/core/java/com/android/server/policy/WindowManagerPolicy.java
@@ -1502,11 +1502,4 @@ public interface WindowManagerPolicy extends WindowManagerPolicyConstants {
      * @return whether the value was changed.
      */
     boolean setAodShowing(boolean aodShowing);
-
-    /**
-     * Long screenshot
-     * @hide
-     */
-    public void takeOPScreenshot(int type);
-    public void stopLongshotConnection();
 }
