638fe173c246995ffde1c16e6858577843783d7a
DarkJoker360 <simoespo159@gmail.com>
2020-04-15 15:09:40 +0000

    Add an option to ask pin for shutdown/reboot on secure lockscreen [1/2]
    Kang Samsung's idea
    
    Based on: https://github.com/ezio84/abc_frameworks_base/commit/62537d0ea76331fe838f25732fbe38c69eb0904e
    https://github.com/crdroidandroid/android_frameworks_base/commit/9aea78f60ebac0d678438f3d86a0ab45d40996a9
    
    @neobuddy89: Code clean up, optimization and disabled by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -135,7 +135,7 @@ public class SmartPixelsTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CRDROID_SETTINGS;
+        return MetricsEvent.SAKURA;
     }
 
     private BroadcastReceiver mSmartPixelsReceiver = new BroadcastReceiver() {
