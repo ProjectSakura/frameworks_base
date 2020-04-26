98ea689b02f5cbf19f84bdb53c7b9a88c02f7c72
LordShenron <shen.priyanshu@gmail.com>
2020-04-26 03:12:35 +0000

    Remove extended audio panel changes to add it again correctly
    This reverts commit eecb5e677d2e8e1329791b394e6122dd833a7e58.
    
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4607,29 +4607,11 @@ public final class Settings {
           */
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
-        /** 
+        /**
          * Show data usage in QS header
          * @hide
          */
         public static final String QS_DATAUSAGE = "qs_datausage";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_RINGER = "audio_panel_view_ringer";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_ALARM = "audio_panel_view_alarm";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_VOICE = "audio_panel_view_voice";
-
-        /** @hide */
-        public static final String AUDIO_PANEL_VIEW_BT_SCO = "audio_panel_view_bt_sco";
 
         /**
          * Whether to change the transparency of the qs panel
