ed59eedaebfba7c5abdee19be8e589316922fa59
HolyAngel <slverwolf@gmail.com>
2020-04-29 23:16:36 +0530

    base: Add Accelerometer and Linear Acceleration sensors in blocking list
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -266,14 +266,21 @@ public class SystemSensorManager extends SensorManager {
                 + "the sensor listeners size has exceeded the maximum limit "
                 + MAX_LISTENER_COUNT);
         }
-        if (sensor.getType() == Sensor.TYPE_SIGNIFICANT_MOTION) {
-            String pkgName = mContext.getPackageName();
-            for (String blockedPkgName : mContext.getResources().getStringArray(
-                    com.android.internal.R.array.config_blockPackagesSensorDrain)) {
-                if (pkgName.equals(blockedPkgName)) {
-                    Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
-                                    "significant motion sensor");
-                    return false;
+
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                 Settings.System.SENSOR_BLOCK, 0) == 1) {
+            int sensortype = sensor.getType();
+            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
+                    sensortype == Sensor.TYPE_ACCELEROMETER ||
+                    sensortype == Sensor.TYPE_LINEAR_ACCELERATION) {
+                String pkgName = mContext.getPackageName();
+                for (String blockedPkgName : mContext.getResources().getStringArray(
+                        com.android.internal.R.array.config_blockPackagesSensorDrain)) {
+                    if (pkgName.equals(blockedPkgName)) {
+                        Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
+                                sensor.getStringType());
+                        return false;
+                    }
                 }
             }
         }
