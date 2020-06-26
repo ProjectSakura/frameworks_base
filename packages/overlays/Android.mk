83abbcc14f3654dabe0781f6230338d692fdf2de
Anirudh Gupta <anirudhgupta109@aosip.dev>
2020-06-26 14:22:40 +0530

    base: overlays: Add Pebble icon shape
    Extracted from coral-user-11-RPB1.200504.018-6520161-release-keys
    
    Change-Id: I258e0596264d623ce1cb5ba7ee3a0ea596892e53
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -58,6 +58,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedThemePickerOverlay \
         IconShapeCylinderOverlay \
 	IconShapeHexagonOverlay \
+	IconShapePebbleOverlay \
 	IconShapeRoundedHexagonOverlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
