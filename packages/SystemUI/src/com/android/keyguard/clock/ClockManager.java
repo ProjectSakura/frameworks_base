94f5b85b5bdef10cc2198f5c7803a594d1d47f1a
Akash Srivastava <akashniki@gmail.com>
2021-02-02 23:26:06 +0000

    Clock: Introduce 404 IDE Clock
    Layout cleanup help by jyotiradityaPanda
    
    Co-authored-by: Rodolphe06 <lokiikol@hotmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -167,6 +167,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new OronosClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
