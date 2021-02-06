def8bc2040f34dba9e6ce06b213e69a8995450d5
Bruno Martins <bgcngm@gmail.com>
2021-02-06 16:45:53 +0100

    Checkout Support for device specific key handlers from Pixelexperience
    Change-Id: Ie65a89cd7efd645622d99d47699df847bc3ad96b
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2247,9 +2247,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 });
 
         final String[] deviceKeyHandlerLibs = res.getStringArray(
-                org.lineageos.platform.internal.R.array.config_deviceKeyHandlerLibs);
+                com.android.internal.R.array.config_deviceKeyHandlerLibs);
         final String[] deviceKeyHandlerClasses = res.getStringArray(
-                org.lineageos.platform.internal.R.array.config_deviceKeyHandlerClasses);
+                com.android.internal.R.array.config_deviceKeyHandlerClasses);
 
         for (int i = 0;
                 i < deviceKeyHandlerLibs.length && i < deviceKeyHandlerClasses.length; i++) {
