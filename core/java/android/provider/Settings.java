99b8ac8291675017d438eec9219bd2a5078463d6
HrX03 <dn.bianco03@gmail.com>
2021-02-15 14:43:39 +0100

    fwb: Implement VolumePluginManager
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5084,6 +5084,12 @@ public final class Settings {
         public static final String AUDIO_PANEL_VIEW_TIMEOUT = "audio_panel_view_timeout";
 
         /**
+         * Holds current enabled volume panel implementation
+         * @hide
+         */
+        public static final String SYSTEMUI_PLUGIN_VOLUME = "systemui_plugin_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
