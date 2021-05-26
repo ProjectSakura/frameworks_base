98750135966be878e2aad3e51e7fd803461e25b0
Pranav Vashi <neobuddy89@gmail.com>
2021-05-26 18:33:42 +0530

    PowerMenu: Dismiss power options before showing restart options
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -2656,6 +2656,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         }
 
         public void showRestartOptionsMenu() { 
+            dismissPowerOptions();
             mRestartOptionsDialog = GlobalActionsPowerDialog.create(
                     mContext, mRestartOptionsAdapter);
             mRestartOptionsDialog.show();
