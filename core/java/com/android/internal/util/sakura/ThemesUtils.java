20f48216aa3675e8185c2201caf7a50f1c4d51e0
Genkzsz11 <Genkzsz11@gmail.com>
2021-06-01 09:07:08 +0200

    Introduce ThemeUtils and add Gvisual mod [1/3]
    co-authored-by: Cruz <du.alexcruz@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

---
+++ core/java/com/android/internal/util/sakura/ThemesUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.internal.util.sakura;
+public class ThemesUtils {
+
+    public static final String[] STATUSBAR_HEIGHT = {
+            "com.gnonymous.gvisualmod.sbh_m", // 1
+            "com.gnonymous.gvisualmod.sbh_l", // 2
+            "com.gnonymous.gvisualmod.sbh_xl", // 3
+    };
+
+    public static final String[] UI_RADIUS = {
+            "com.gnonymous.gvisualmod.urm_r", // 1
+            "com.gnonymous.gvisualmod.urm_m", // 2
+            "com.gnonymous.gvisualmod.urm_l", // 3
+    };
+}
