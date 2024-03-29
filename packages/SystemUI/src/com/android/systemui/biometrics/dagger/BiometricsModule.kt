4aef2929129c8e75ebb699239c5c106caee49567
ReveRTX <rev9rt@gmail.com>
2024-03-29 17:47:02 +0100

    Revert "fixup! SystemUI: Add FingerprintInteractiveToAuthProvider implementation"
    This reverts commit 9b1052456aa78d3b807e6d174d36fb1f7fe3c66c.

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -22,8 +22,6 @@ import android.content.res.Resources
 import com.android.internal.R
 import com.android.systemui.CoreStartable
 import com.android.systemui.biometrics.EllipseOverlapDetectorParams
-import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
-import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.biometrics.UdfpsUtils
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepository
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepositoryImpl
@@ -42,10 +40,9 @@ import com.android.systemui.biometrics.udfps.EllipseOverlapDetector
 import com.android.systemui.biometrics.udfps.OverlapDetector
 import com.android.systemui.biometrics.ui.binder.SideFpsOverlayViewBinder
 import com.android.systemui.dagger.SysUISingleton
-import com.android.systemui.dagger.qualifiers.Background
-import com.android.systemui.user.domain.interactor.SelectedUserInteractor
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProviderImpl
 import com.android.systemui.util.concurrency.ThreadFactory
-import com.android.systemui.util.settings.SecureSettings
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -53,7 +50,6 @@ import dagger.multibindings.ClassKey
 import dagger.multibindings.IntoMap
 import java.util.concurrent.Executor
 import javax.inject.Qualifier
-import kotlinx.coroutines.CoroutineDispatcher
 
 /** Dagger module for all things biometric. */
 @Module
@@ -124,19 +120,8 @@ interface BiometricsModule {
         }
 
         @Provides
-        @Background
-        fun providesFingerprintInteractiveToAuth(
-            backgroundDispatcher: CoroutineDispatcher,
-            context: Context,
-            secureSettings: SecureSettings,
-            selectedUserInteractor: SelectedUserInteractor,
-        ): FingerprintInteractiveToAuthProvider =
-            FingerprintInteractiveToAuthProviderImpl(
-                backgroundDispatcher,
-                context,
-                secureSettings,
-                selectedUserInteractor,
-            )
+        fun providesFingerprintInteractiveToAuth(ctx: Context): FingerprintInteractiveToAuthProvider =
+            FingerprintInteractiveToAuthProviderImpl(ctx);
     }
 }
 
