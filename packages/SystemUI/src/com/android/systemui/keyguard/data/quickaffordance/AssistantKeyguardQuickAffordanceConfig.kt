3168c7dac853d78bc2e00142e56076af97ffc055
aswin7469 <aswinas@pixysos.com>
2024-06-28 00:25:15 +0530

    SystemUI: Update ChatGPT quickaffordance activity
    * changed on chatgpt app v1.2024.170+
    
    Change-Id: I2817c6134bebd95268ca5ad39f2f18a27a8ca2fc
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/AssistantKeyguardQuickAffordanceConfig.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/data/quickaffordance/AssistantKeyguardQuickAffordanceConfig.kt
@@ -31,7 +31,7 @@ import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
-import com.android.internal.util.crdroid.Utils
+import com.android.internal.util.sakura.Utils
 
 @SysUISingleton
 class AssistantKeyguardQuickAffordanceConfig
@@ -71,7 +71,7 @@ constructor(
     override fun onTriggered(
         expandable: Expandable?,
     ): KeyguardQuickAffordanceConfig.OnTriggeredResult {
-        val intent = Intent().setClassName("com.openai.chatgpt", "com.openai.voice.VoiceModeActivity")
+        val intent = Intent().setClassName("com.openai.chatgpt", "com.openai.voice.assistant.AssistantActivity")
         return KeyguardQuickAffordanceConfig.OnTriggeredResult.StartActivity(
             intent = intent,
             canShowWhileLocked = true,
