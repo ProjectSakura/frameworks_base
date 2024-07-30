1b41fec9bb54547c6e6f5f8c71b6826ec5662187
Pranav Vashi <neobuddy89@gmail.com>
2024-07-31 00:42:34 +0530

    PhoneWindowManager: Check NPE for LineageHardware
    * LineageHardware is initialized in systemReady()
    * If user is switched before systemReady(), mMultiuserReceiver will
      invoke mSettingsObserver.onChange(false); resulting in below crash
    
    07-30 11:44:41.772 E/AndroidRuntime( 2056): *** FATAL EXCEPTION IN SYSTEM PROCESS: main
    07-30 11:44:41.772 E/AndroidRuntime( 2056): java.lang.RuntimeException: Error receiving broadcast Intent { act=android.intent.action.USER_SWITCHED flg=0x50000010 (has extras) } in com.android.server.policy.PhoneWindowManager$17@e24db87
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0(LoadedApk.java:1818)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.app.LoadedApk$ReceiverDispatcher$Args.$r8$lambda$mcNAAl1SQ4MyJPyDg8TJ2x2h0Rk(Unknown Source:0)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.app.LoadedApk$ReceiverDispatcher$Args$$ExternalSyntheticLambda0.run(D8$$SyntheticClass:0)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.os.Handler.handleCallback(Handler.java:959)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.os.Handler.dispatchMessage(Handler.java:100)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.os.Looper.loopOnce(Looper.java:232)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.os.Looper.loop(Looper.java:317)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.SystemServer.run(SystemServer.java:1030)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.SystemServer.main(SystemServer.java:714)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at java.lang.reflect.Method.invoke(Native Method)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:583)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:856)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'boolean lineageos.hardware.LineageHardwareManager.isSupported(int)' on a null object reference
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.policy.PhoneWindowManager.updateSettings(PhoneWindowManager.java:3477)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.policy.PhoneWindowManager.updateSettings(PhoneWindowManager.java:3368)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.policy.PhoneWindowManager.-$$Nest$mupdateSettings(PhoneWindowManager.java:0)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.policy.PhoneWindowManager$SettingsObserver.onChange(PhoneWindowManager.java:1136)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at com.android.server.policy.PhoneWindowManager$17.onReceive(PhoneWindowManager.java:6323)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0(LoadedApk.java:1810)
    07-30 11:44:41.772 E/AndroidRuntime( 2056): 	... 11 more
    
    * We either guard settingsObserver or LineageHardware.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3462,7 +3462,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_CURRENT);
             if (forceNavbar != mForceNavbar) {
                 mForceNavbar = forceNavbar;
-                if (mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
+                if (mLineageHardware != null && mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_DISABLE)) {
                     mLineageHardware.set(LineageHardwareManager.FEATURE_KEY_DISABLE,
                             mForceNavbar == 1);
                 }
