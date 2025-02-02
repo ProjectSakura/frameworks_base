1ed7bc7ba1b7b0e0c429e13328368120a4101feb
Ido Ben-Hur <idoybh2@gmail.com>
2025-02-02 14:13:43 +0530

    base: Add a toggle for long press on navbar to search [1/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6956,6 +6956,13 @@ public final class Settings {
         public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
 
         /**
+         * Whether long press on navbar gesture is enabled
+         * @hide
+         */
+        @Readable
+        public static final String NAVBAR_LONG_PRESS_GESTURE = "navbar_long_press_gesture";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
