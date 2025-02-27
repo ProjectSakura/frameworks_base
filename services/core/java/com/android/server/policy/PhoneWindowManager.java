772410700ea47dff1f558256976e67238ef20533
ArmSM <armsmbuild1@gmail.com>
2025-02-27 15:20:00 +0100

    Revert "PhoneWindowManager: Support volume up/down to mute"
    This reverts commit 88cbfc20e4a03392fc758e0b75e7a5b1ede4f395.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -672,9 +672,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Click volume down + power for partial screenshot
     boolean mClickPartialScreenshot;
 
-    // Volume Up and Down to mute on Android TV
-    boolean mVolUpAndDownMute;
-
     private boolean mPendingKeyguardOccluded;
     private boolean mKeyguardOccludedChanged;
 
@@ -865,8 +862,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private LineageHardwareManager mLineageHardware;
 
     private boolean mLongSwipeDown;
-    private CameraAvailbilityListener mCameraAvailabilityListener;
-
     private CameraAvailbilityListener mCameraAvailabilityListener;
 
     private class PolicyHandler extends Handler {
@@ -1137,8 +1132,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
-            resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                    LineageSettings.System.VOLUME_UP_AND_DOWN_MUTE), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -2868,39 +2861,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
 
-        if (mHasFeatureLeanback) {
-            mKeyCombinationManager.addRule(
-                    new TwoKeysCombinationRule(KEYCODE_VOLUME_DOWN, KEYCODE_VOLUME_UP) {
-                        @Override
-                        boolean preCondition() {
-                            return mVolUpAndDownMute;
-                        }
-                        @Override
-                        void execute() {
-                            triggerVirtualKeypress(KeyEvent.KEYCODE_VOLUME_MUTE);
-                        }
-                        @Override
-                        void cancel() {
-                        }
-                    });
-        } else {
-            mKeyCombinationManager.addRule(
-                    new TwoKeysCombinationRule(KEYCODE_VOLUME_DOWN, KEYCODE_VOLUME_UP) {
-                        @Override
-                        boolean preCondition() {
-                            return mAccessibilityShortcutController
-                                    .isAccessibilityShortcutAvailable(isKeyguardLocked());
-                        }
-                        @Override
-                        void execute() {
-                            interceptAccessibilityShortcutChord();
-                        }
-                        @Override
-                        void cancel() {
-                            cancelPendingAccessibilityShortcutAction();
-                        }
-                    });
-        }
+        mKeyCombinationManager.addRule(
+                new TwoKeysCombinationRule(KEYCODE_VOLUME_DOWN, KEYCODE_VOLUME_UP) {
+                    @Override
+                    boolean preCondition() {
+                        return mAccessibilityShortcutController
+                                .isAccessibilityShortcutAvailable(isKeyguardLocked());
+                    }
+                    @Override
+                    void execute() {
+                        interceptAccessibilityShortcutChord();
+                    }
+                    @Override
+                    void cancel() {
+                        cancelPendingAccessibilityShortcutAction();
+                    }
+                });
 
         // Volume up + power can either be the "ringer toggle chord" or as another way to
         // launch GlobalActions. This behavior can change at runtime so we must check behavior
@@ -3476,9 +3452,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             UserHandle.USER_CURRENT) == 1;
             mCameraLaunch = LineageSettings.System.getIntForUser(resolver,
                     LineageSettings.System.CAMERA_LAUNCH, 0,
-                    UserHandle.USER_CURRENT) == 1;
-            mVolUpAndDownMute = LineageSettings.System.getIntForUser(resolver,
-                    LineageSettings.System.VOLUME_UP_AND_DOWN_MUTE, 0,
                     UserHandle.USER_CURRENT) == 1;
 
             // Configure wake gesture.
