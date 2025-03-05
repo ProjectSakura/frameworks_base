b2980c44ccbaf66c43823b63a200ed951df4f07b
François Gaffie <francois.gaffie@ampere.cars>
2025-03-05 11:23:02 +0100

    audiomanager: fix orphan focus holder when using ext focus policy
    When an focus management is delegate to an external Audio Policy, the
    answer to the request is expected within 250ms. If the timer expires,
    it is rejected. Timer is armed on client side.
    It may lead to orphan focus owner as ext AudioPolicy may be temporary
    stuck and would manage the request anyway.
    
    This CL fixes the orphan owner by abandonning any timed out request.
    
    Bug: 400752237
    Test: m
    
    Google: 3527210
    Change-Id: I0b4e60475e83b8faa58068fe2b7753c6fbd6df7c
    Signed-off-by: François Gaffie <francois.gaffie@ampere.cars>

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -4742,7 +4742,7 @@ public class AudioManager {
             focusReceiver = addClientIdToFocusReceiverLocked(clientFakeId);
         }
 
-        return handleExternalAudioPolicyWaitIfNeeded(clientFakeId, focusReceiver);
+        return handleExternalAudioPolicyWaitIfNeeded(clientFakeId, focusReceiver, afr);
     }
 
     /**
@@ -4955,7 +4955,7 @@ public class AudioManager {
             focusReceiver = addClientIdToFocusReceiverLocked(clientId);
         }
 
-        return handleExternalAudioPolicyWaitIfNeeded(clientId, focusReceiver);
+        return handleExternalAudioPolicyWaitIfNeeded(clientId, focusReceiver, afr);
     }
 
     @GuardedBy("mFocusRequestsLock")
@@ -4971,11 +4971,20 @@ public class AudioManager {
     }
 
     private @FocusRequestResult int handleExternalAudioPolicyWaitIfNeeded(String clientId,
-            BlockingFocusResultReceiver focusReceiver) {
+            BlockingFocusResultReceiver focusReceiver, @NonNull AudioFocusRequest afr) {
         focusReceiver.waitForResult(EXT_FOCUS_POLICY_TIMEOUT_MS);
-        if (DEBUG && !focusReceiver.receivedResult()) {
-            Log.e(TAG, "handleExternalAudioPolicyWaitIfNeeded"
-                    + " response from ext policy timed out, denying request");
+        if (!focusReceiver.receivedResult()) {
+            if (DEBUG) {
+                Log.e(TAG, "handleExternalAudioPolicyWaitIfNeeded"
+                        + " response from ext policy timed out, denying request");
+            }
+            try {
+                // To prevent from orphan focus holder, cleanup
+                abandonAudioFocus(afr.getOnAudioFocusChangeListener());
+            } catch (Exception e) {
+                Log.e(TAG, "handleExternalAudioPolicyWaitIfNeeded failed to abandon audio"
+                        +" focus after time out, error: " + e.getMessage());
+            }
         }
 
         synchronized (mFocusRequestsLock) {
