0823d38297fe09353092144d19bd32f66d3fc2e4
Pranav Vashi <neobuddy89@gmail.com>
2025-04-03 09:22:46 +0530

    Partially revert "Fix for the lock icon issue"
    Ref commit: https://github.com/crdroidandroid/android_frameworks_base/commit/3c9940c668209b9adf05586c4a409b8c5ee0457b
    
    Reason for revert:
    
    Image state should be set before setting padding else
    icon would set padding with previous configuration.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/DeviceEntryIconViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/binder/DeviceEntryIconViewBinder.kt
@@ -55,7 +55,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.SharingStarted
 import android.provider.Settings
 import android.os.UserHandle
-import com.android.internal.util.crdroid.Utils
+import com.android.internal.util.sakura.Utils
 
 object DeviceEntryIconViewBinder {
     private const val TAG = "DeviceEntryIconViewBinder"
@@ -265,6 +265,10 @@ object DeviceEntryIconViewBinder {
                     launch("$TAG#fpIconView.viewModel") {
                         fgViewModel.viewModel.collect { viewModel ->
                             Log.d(TAG, "Updating device entry icon image state $viewModel")
+                            fgIconView.setImageState(
+                                view.getIconState(viewModel.type, viewModel.useAodVariant),
+                                /* merge */ false,
+                            )
                             if (viewModel.type.contentDescriptionResId != -1) {
                                 fgIconView.contentDescription =
                                     fgIconView.resources.getString(
@@ -283,12 +287,6 @@ object DeviceEntryIconViewBinder {
                             } else {
                                 fgIconView.setPadding(0, 0, 0, 0)
                             }
-                            // Set image state at the end after updating other view state. This
-                            // method forces the ImageView to recompute the bounds of the drawable.
-                            fgIconView.setImageState(
-                                view.getIconState(viewModel.type, viewModel.useAodVariant),
-                                /* merge */ false,
-                            )
                             // Invalidate, just in case the padding changes just after icon changes
                             fgIconView.invalidate()
                         }
