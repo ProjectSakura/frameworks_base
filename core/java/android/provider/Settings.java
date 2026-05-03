16b6a62e1cbb11156b5c28fb3fed85a8dfa7944b
Pranav Vashi <neobuddy89@gmail.com>
2026-05-03 08:50:12 +0530

    Pulse: Add subtle haptics on bass spikes
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14246,6 +14246,11 @@ public final class Settings {
         public static final String PULSE_RENDERER = "pulse_renderer";
 
         /**
+         * @hide
+         */
+        public static final String PULSE_BASS_HAPTICS = "pulse_bass_haptics";
+
+        /**
          * Whether to show or hide the arrow for back gesture
          * @hide
          */
