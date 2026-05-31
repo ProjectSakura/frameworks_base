ca600efd4fc621a508b8d8707f4a2a5f9d5b6dc1
Ghosuto <clash.raja10@gmail.com>
2026-05-31 06:24:14 +0000

    SystemUI: Replace MSDL haptics with native haptic feedback on tiles
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/Tile.kt
+++ packages/SystemUI/src/com/android/systemui/qs/panels/ui/compose/infinitegrid/Tile.kt
@@ -27,6 +27,7 @@ import android.provider.Settings
 import android.service.quicksettings.Tile.STATE_ACTIVE
 import android.service.quicksettings.Tile.STATE_INACTIVE
 import android.service.quicksettings.Tile.STATE_UNAVAILABLE
+import android.view.HapticFeedbackConstants
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -80,6 +81,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalResources
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.role
@@ -212,7 +214,7 @@ fun ContentScope.Tile(
 
         val colors = TileDefaults.getColorForState(uiState, iconOnly)
         val hapticsViewModel: TileHapticsViewModel? =
-            if (rememberTileHaptic()) {
+            if (rememberTileHaptic() && !Flags.msdlFeedback()) {
                 rememberViewModel(traceName = "TileHapticsViewModel") {
                     tileHapticsViewModelFactoryProvider.getHapticsViewModelFactory()?.create(tile)
                 }
@@ -220,6 +222,8 @@ fun ContentScope.Tile(
                null
             }
 
+        val view = LocalView.current
+        val hapticEnabled = rememberTileHaptic()
         val classicStyle = rememberQSPanelStyle()
         val iconShapeKey = rememberQSTileIconShapeKey()
         val labelHide = classicStyle && rememberQSTileLabelHide()
@@ -288,9 +292,9 @@ fun ContentScope.Tile(
             val useLongClickToSettings = !(iconOnly && isDualTarget && isClickable)
             val longClick: (() -> Unit)? =
                 {
-                        hapticsViewModel?.setTileInteractionState(
-                            TileHapticsViewModel.TileInteractionState.LONG_CLICKED
-                        )
+                        if (hapticEnabled) {
+                            view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
+                        }
 
                         if (useLongClickToSettings) {
                             tile.settingsClick(expandable)
@@ -325,9 +329,9 @@ fun ContentScope.Tile(
                         }
 
                         // Side effects of the click
-                        hapticsViewModel?.setTileInteractionState(
-                            TileHapticsViewModel.TileInteractionState.CLICKED
-                        )
+                        if (hapticEnabled) {
+                            view.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK)
+                        }
 
                         coroutineScope.launch {
                             // Bounce the tile's container if it is toggleable and is not a large
@@ -429,9 +433,9 @@ fun ContentScope.Tile(
                         val iconShape by TileDefaults.animateIconShapeAsState(uiState.state, shapeMode)
                         val secondaryClick: (() -> Unit)? =
                             {
-                                    hapticsViewModel?.setTileInteractionState(
-                                        TileHapticsViewModel.TileInteractionState.CLICKED
-                                    )
+                                    if (hapticEnabled) {
+                                        view.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK)
+                                    }
                                     tile.toggleClick()
                                 }
                                 .takeIf { isDualTarget }
@@ -587,7 +591,7 @@ fun Modifier.tileCombinedClickable(
             onLongClick = onLongClick,
             onClickLabel = accessibilityUiState.clickLabel,
             onLongClickLabel = longPressLabel,
-            hapticFeedbackEnabled = rememberTileHaptic() && !Flags.msdlFeedback(),
+            hapticFeedbackEnabled = false,
             interactionSource = interactionSource,
         )
         .semantics {
