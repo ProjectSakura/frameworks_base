1fdd2306054268cf06e513271ac6b604477f8e9d
Ghosuto <clash.raja10@gmail.com>
2026-08-08 07:45:51 +0000

    SystemUI: Fix media gap not collapsing when lockscreen media disable
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/KeyguardMediaController.kt
+++ packages/SystemUI/src/com/android/systemui/media/controls/ui/controller/KeyguardMediaController.kt
@@ -61,6 +61,7 @@ import java.io.PrintWriter
 import javax.inject.Inject
 import javax.inject.Named
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 
@@ -121,6 +122,7 @@ constructor(
 
             // Let's now initialize this view, which also creates the host view for us.
             mediaHost.init(MediaHierarchyManager.LOCATION_LOCKSCREEN)
+            listenForLockscreenMediaAllowedChanges(applicationScope)
         } else {
             applicationScope.launch {
                 combine(
@@ -156,6 +158,15 @@ constructor(
             }
         )
         updateResources()
+    }
+
+    @VisibleForTesting
+    internal fun listenForLockscreenMediaAllowedChanges(scope: CoroutineScope): Job {
+        return scope.launch {
+            mediaCarouselInteractor.allowMediaOnLockscreen.collect {
+                refreshMediaPosition(reason = "lockscreen media allowed changed")
+            }
+        }
     }
 
     private fun setComposeContent(composeView: ComposeView) {
@@ -307,7 +318,8 @@ constructor(
             if (MediaControlsInComposeFlag.isEnabled) {
                 isMediaVisibleOnLockscreen
             } else {
-                mediaHost.visible
+                val allowedOnLockscreen = mediaCarouselInteractor.allowMediaOnLockscreen.value
+                mediaHost.visible && allowedOnLockscreen
             }
         val isBypassNotEnabled = !bypassController.bypassEnabled
         val useSplitShade = useSplitShade
